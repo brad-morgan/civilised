4052e8d6d0ee7902faa48e7aa90047163f4297f3
Brad Morgan <brad@bradmorgan.co.nz>
2015-05-27 14:24:10 +1200

    Added test to make sure ImportController.Index() returns the Import/Index view.
    Test currently doesn't pass due to the need for the controller to inherit from the MVC Controller class. Committing test code before I fix this.

---
+++ Civilised/Civilised.Tests/ImportControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Civilised.Controllers;
+using System.Web.Mvc;
+using Xunit;
+
+namespace Civilised.Tests
+{
+   public class ImportControllerTests
+    {
+       [Fact(Skip="ImportController needs to inherit from Controller to call View()."]
+       public void IndexActionReturnsIndexView()
+       {
+           var controller = new ImportController();
+           var result = controller.Index() as ViewResult;
+           Assert.Equal("Index", result.ViewName);
+       }
+    }
+}
