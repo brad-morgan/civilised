f7b3a36c813717a4c40fd865df93c3126d90b0b9
Brad Morgan <brad@bradmorgan.co.nz>
2015-06-18 15:51:16 +1200

    ArgumentNullException us thrown when ColumnDefinition.Name is null

---
+++ Civilised/Civilised.Tests/ColumnDefinitionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Civilised.Services;
+
+namespace Civilised.Tests
+{
+   public class ColumnDefinitionTests
+    {
+       [Fact]
+       public void ArgumentNullExceptionWhenNameIsSetToNull()
+       {
+           var columnDefinition = new ColumnDefinition();
+           Assert.Throws<ArgumentNullException>(() => { columnDefinition.Name = null; });
+       }
+    }
+}
