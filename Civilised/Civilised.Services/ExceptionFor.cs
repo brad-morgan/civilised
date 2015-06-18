5f67ab059111900aee640d2c7de86a9c944b7aa0
Brad Morgan <brad@bradmorgan.co.nz>
2015-06-18 17:19:08 +1200

    ArgumentException is thrown when Name is empty or whitespace.
    Moved null exception logic into ExceptionFor and created ExceptionFor.NullEmptyOrWhitepace since most strings are checked for all three cases.

---
+++ Civilised/Civilised.Services/ExceptionFor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Civilised.Services
+{
+    /// <summary>
+    /// Builds appropriate exceptions for common invalid data scenarios.
+    /// </summary>
+   internal static class ExceptionFor
+    {
+       internal static Exception NullEmptyOrWhitespace(ref string field, string fieldName)
+       {
+           if(field == null)
+           {
+               return new ArgumentNullException(fieldName);
+           }
+           if (field.Trim().Length < 1)
+           {
+               return new ArgumentException("Must contain at least one non-whitespace character", fieldName);
+           }
+           return null;
+       }
+    }
+}
--- Civilised/Civilised.Tests/ColumnDefinitionTests.cs
+++ Civilised/Civilised.Tests/ColumnDefinitionTests.cs
@@ -10,11 +10,22 @@ namespace Civilised.Tests
 {
    public class ColumnDefinitionTests
     {
+       public static readonly object[] EmptyAndWhitespaceStrings = new object[]{string.Empty, " \t "};
+
        [Fact]
        public void ArgumentNullExceptionWhenNameIsSetToNull()
        {
            var columnDefinition = new ColumnDefinition();
            Assert.Throws<ArgumentNullException>(() => { columnDefinition.Name = null; });
        }
+       [Theory]
+       [InlineData("")]
+       [InlineData(" \t ")]
+       public void ArgumentExceptionWhenNameIsSetToEmptyOrWhitespace(string emptyOrWhiteSpace)
+       {
+           var columnDefinition = new ColumnDefinition();
+           Assert.Throws<ArgumentException>(() => { columnDefinition.Name = emptyOrWhiteSpace; });
+       }
+
     }
 }
