16c70487929e74676b58e4ff5cc2a328caa91466
Brad Morgan <brad@bradmorgan.co.nz>
2015-06-27 09:42:50 +1200

    TimetableEvent properties which are required are marked with the Required attribute.
    This attribute is used by ASP.NET to determine whether the absence of a value is appropriate for a given property.

---
+++ Civilised/Civilised.Tests/TimetableEventTests.cs
@@ -0,0 +1,32 @@
+﻿using Civilised.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Civilised.Tests
+{
+   public class TimetableEventTests
+    {
+       [Theory]
+       [InlineData("CourseCode")]
+       [InlineData("Description")]
+       [InlineData("StartDate")]
+       [InlineData("EndDate")]
+       [InlineData("StartTime")]
+       [InlineData("EndTime")]
+       [InlineData("Location")]
+       public void RequiredPropertiesAreAnnotated(string propertyName)
+       {
+           //TODO: Q & A about reflection, and BindingFlags
+           var property = typeof(TimetableEvent).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+           var attributes = property.GetCustomAttributes(typeof(RequiredAttribute));
+           Assert.NotEmpty(attributes);
+       }
+    }
+}
---
+++ Civilised/Civilised/Models/TimetableEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Civilised.Models
+{
+    /// <summary>
+    /// Represents a single timetable event.
+    /// </summary>
+    public class TimetableEvent
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the course associated with this event.
+        /// </summary>
+        /// <remarks>This is the CPIT course code for the course associated with this event.</remarks>
+        /// <example>IS206</example>
+        [Required]
+        public string CourseCode { get; set; }
+        /// <summary>
+        /// Gets or sets a description of the course associated with this event.
+        /// </summary>
+        /// <remarks>This is the title of the course associated with this event.</remarks>
+        /// <example>Professional Practice.</example>
+        [Required]
+        public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the date of the first occurance of this event.
+        /// </summary>
+        [Required]
+        public DateTime StartDate { get; set; }
+        /// <summary>
+        /// Gets or sets the date of the last occurance of this event.
+        /// </summary>
+        [Required]
+        public DateTime EndDate { get; set; }
+        /// <summary>
+        /// Gets or sets the time at which this event starts
+        /// </summary>
+        [Required]
+        public DateTime StartTime { get; set; }
+        /// <summary>
+        /// Gets or sets the time at which this event ends.
+        /// </summary>
+        [Required]
+        public DateTime EndTime { get; set; }
+        /// <summary>
+        /// Gets or sets the identifier for the location in which this event takes place.
+        /// </summary>
+        /// <remarks>This is the room identifier for the location where the event takes place</remarks>
+        /// <example>L203</example>
+        [Required]
+        public string Location { get; set; }
+    }
+}
