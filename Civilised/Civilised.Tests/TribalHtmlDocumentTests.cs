5bd14bc9c08dae8fa1f07f1f09745ffb3e0573f7
Brad Morgan <brad@bradmorgan.co.nz>
2015-06-27 15:04:11 +1200

    After Load is called Columns contains ColumnDefinition object representing each column found in the source data.

---
+++ Civilised/Civilised.Tests/TribalHtmlDocumentTests.cs
@@ -0,0 +1,33 @@
+﻿using Civilised.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Civilised.Tests
+{
+    public class TribalHtmlDocumentTests
+    {
+        [Fact]
+        public void IsATimetableReader()
+        {
+            Assert.IsAssignableFrom<TimetableDocument>(new TribalHtmlTimetableDocument());
+        }
+        [Fact]
+        public void AfterFirstReadColumnsContainsColumnNamesFromSource()
+        {
+            var expectedColumnNames = new string[] { "Session", "Event Type", "Course(s)", "Description", "Day of Week", "Start Date", "End Date", "Start Time", "End Time", "Room" };
+            using (var source = new MemoryStream(Encoding.UTF8.GetBytes(Properties.Resources.TribalTimetableHtml)))
+            {
+                var document = new TribalHtmlTimetableDocument();
+                document.Load(source);
+                var actualColumnNames = document.Columns.Select(column => { return column.Name; });
+                Assert.Equal(expectedColumnNames, actualColumnNames);
+            }
+
+        }
+    }
+}
---
+++ Civilised/Civilised/Services/TimetableDocument.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Civilised.Services
+{
+    /// <summary>
+    /// The base class for any class which is able to read a timetable and produce a collection of <see cref="TimetableEvent"/> instances matching those events.
+    /// </summary>
+    public abstract class TimetableDocument
+    {
+        
+        /// <summary>
+        /// Gets a collection of <see cref="ColumnDefinition"/> objects representing the columns present in the source timetable data.
+        /// </summary>
+        public abstract IEnumerable<ColumnDefinition> Columns
+        {
+            get;
+        }
+        public abstract IEnumerable<TimetableDocument> Events
+        {
+            get;
+        }
+        /// <summary>
+        /// Loads HTML from the specified stream.
+        /// </summary>
+        /// <param name="stream">The stream containing the timetable HTML to be parsed.</param>
+        public abstract void Load(Stream stream);
+    }
+}
