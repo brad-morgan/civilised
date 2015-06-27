7f7b3a0e8de430af9324373244e02deca8d7b756
Brad Morgan <brad@bradmorgan.co.nz>
2015-06-27 19:58:20 +1200

    Cleaned up initialization code in InstancesWithSamePropertyValuesHaveSameHashCode

--- Civilised/Civilised.Tests/TimetableEventTests.cs
+++ Civilised/Civilised.Tests/TimetableEventTests.cs
@@ -32,23 +32,27 @@ namespace Civilised.Tests
        public void InstancesWithSamePropertyValuesHaveSameHashCode()
        {
            //FIXME: Reduce setup code size. Possibly a test data class?
-           var instanceA = new TimetableEvent();
-           var instanceB = new TimetableEvent();
-           instanceA.CourseCode = "IS206";
-           instanceA.Description = "Professional Practice";
-           instanceA.EndDate = DateTime.Today.AddDays(1);
-           instanceA.EndTime = DateTime.Today.AddHours(15);
-           instanceA.Location = "L203";
-           instanceA.StartDate = DateTime.Today;
-           instanceA.StartTime = DateTime.Today.AddHours(13);
-
-           instanceB.CourseCode = "IS206";
-           instanceB.Description = "Professional Practice";
-           instanceB.EndDate = DateTime.Today.AddDays(1);
-           instanceB.EndTime = DateTime.Today.AddHours(15);
-           instanceB.Location = "L203";
-           instanceB.StartDate = DateTime.Today;
-           instanceB.StartTime = DateTime.Today.AddHours(13);
+           var instanceA = new TimetableEvent() 
+           {
+               CourseCode = "IS206",
+               Description = "Professional Practice",
+               StartDate=DateTime.Today,
+               EndDate=DateTime.Today.AddDays(7*10),
+               StartTime=DateTime.Now,
+               EndTime=DateTime.Now.AddHours(2),
+               Location="L210"
+           };
+           var instanceB = new TimetableEvent()
+           {
+               CourseCode = "IS206",
+               Description = "Professional Practice",
+               StartDate = DateTime.Today,
+               EndDate = DateTime.Today.AddDays(7 * 10),
+               StartTime = DateTime.Now,
+               EndTime = DateTime.Now.AddHours(2),
+               Location = "L210"
+           };
+           
            Assert.Equal(instanceA.GetHashCode(), instanceB.GetHashCode());
        }
        //FIXME: Need to add tests to do range checks on dates and times. e.g. StartTime can't be after end time etc.
